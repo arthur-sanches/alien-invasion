bb89c519257ec8d481b333430c3a7a04c58a48cb
arthur-sanches <arthur.sanches96@gmail.com>
2022-04-23 21:26:47 -0300

    Adds a game screen and a ship with movement

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.screen_size = (self.screen_width, self.screen_height)
+        self.bg_color = (208, 208, 208)
